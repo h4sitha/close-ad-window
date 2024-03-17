abea3bf0036d05448450644025dd28642ab0cf43
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-03-17 12:22:10 +0530

    Add reset button when the ad closes

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const closeBtn = document.querySelector('svg');
 const elementsContainer = document.querySelector('div');
+const body = document.querySelector('body');
+
+const resetBtn = document.createElement('button');
 
 closeBtn.addEventListener('click', () => {
     elementsContainer.remove();
+    resetBtn.textContent = "Reset";
+    body.appendChild(resetBtn);
 })
